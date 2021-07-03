2dd0acf3fea7411de99610f7c79ac6b1c35917be
cristysandu <crystisandu3@gmail.com>
2021-07-03 17:41:04 +0300

    add app with moview models and api call

---
+++ android/app/src/main/kotlin/com/example/yts_movies_gat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yts_movies_gat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/models/movie.dart
@@ -0,0 +1,22 @@
+library movie;
+
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:yts_movies_gat/src/models/serializers.dart';
+
+
+part 'movie.g.dart';
+
+abstract class Movie implements Built<Movie, MovieBuilder> {
+  factory Movie([void Function(MovieBuilder) updates]) = _$Movie;
+
+  factory Movie.fromJson(dynamic json) {
+    return serializers.deserializeWith(serializer, json) as Movie;
+  }
+
+  Movie._();
+
+  String get title;
+
+  static Serializer<Movie> get serializer => _$movieSerializer;
+}
